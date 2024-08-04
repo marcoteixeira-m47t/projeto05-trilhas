f89304fd0bf26654a1829d2c48f307cc4cdb3281
Marco Teixeira <marcoteixeira.m47t@gmail.com>
2024-08-04 19:20:13 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const destinationRoutes = require('./routes/destinations');
-
-app.use('/api/destinations', destinationRoutes);
-
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
-});
